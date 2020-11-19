e2fb7c0cf9d8b3c7a75d96dcbc12e05317891a53
ActiveOne/DanielMarín <daniel.marin@activeone.com>
2020-11-19 09:38:35 -0500

    Use hooks and async await

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [personajes, setPersonajes] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://swapi.dev/api/people/")
+      const data = await response.json();
+      setPersonajes(data.results)
+    }
+    fetchData()
+  }, [])
+
+  console.log(personajes)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <ol>
+          {personajes.map((personaje) => (
+            <ul key={personaje.name}>{personaje.name}</ul>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/*
+x 1) pasar a hooks
+x 2) usar async await
+  3) crear un componente reusable para cada personaje
+  4) de cada personaje vas a mostrar el nombre, peso, color de cabello y el color de los ojos
+  5) en el estado podemos crear nuevos personajes, actualizarlos, eliminarlos
+  6) repositorio en GitHub y lo mandas a mi correo
+*/
